f52515fc899fc85339ba5cdace5dfe4bfff61f7f
Carly Schmitting <cschmitting@gmail.com>
2020-08-17 15:57:11 +0200

    add contact, about and compose pages

--- app.js
+++ app.js
@@ -15,19 +15,28 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", function(req, res){
+  res.render("home", {homeStartingContent: homeStartingContent});
+});
 
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
 
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
 
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
 
+app.post("/compose", function(req, res) {
+  const post = { title: req.body.postTitle, content: req.body.postBody };
+  console.log(post);
+  res.redirect("/");
+});
 
-
-
-
-
-
-
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(3000, function(){
+  console.log("Gotchu on port 3000, gurl");
 });
